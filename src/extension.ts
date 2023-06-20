6f13ee78d19e70e6cf87b98c84e4c4d6db46c7aa
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-06-19 21:28:50 -0700

    feat: nextls

--- src/extension.ts
+++ src/extension.ts
@@ -10,10 +10,11 @@ import {
 } from "vscode-languageclient/node";
 
 let credoClient: LanguageClient;
+let nextLSClient: LanguageClient;
 
-async function latestRelease(): Promise<string> {
+async function latestRelease(project: string): Promise<string> {
   return fetch(
-    "https://api.github.com/repos/elixir-tools/credo-language-server/releases/latest",
+    `https://api.github.com/repos/elixir-tools/${project}/releases/latest`,
     {
       headers: {
         ["X-GitHub-Api-Version"]: "2022-11-28",
@@ -25,68 +26,139 @@ async function latestRelease(): Promise<string> {
     .then((x: any): string => x.tag_name.replace(/^v/, ""));
 }
 
-export async function activate(context: vscode.ExtensionContext) {
-  let files = await vscode.workspace.findFiles("mix.exs");
-
+async function activateCredo(
+  context: vscode.ExtensionContext,
+  mixfile: vscode.Uri
+) {
   let config = vscode.workspace.getConfiguration("elixir-tools.credo");
+  let text = await vscode.workspace.fs.readFile(mixfile);
 
-  if (files[0]) {
-    let text = await vscode.workspace.fs.readFile(files[0]);
+  if (text.toString().includes("{:credo")) {
+    if (config.get("enable")) {
+      let serverOptions: ServerOptions;
 
-    if (text.toString().includes("{:credo")) {
-      if (config.get("enable")) {
-        let serverOptions: ServerOptions;
-
-        switch (config.get("adapter")) {
-          case "stdio":
-            serverOptions = {
-              options: {
-                env: Object.assign({}, process.env, {
-                  ["CREDO_LSP_VERSION"]: await latestRelease(),
-                }),
-              },
-              command: context.asAbsolutePath("./bin/credo-language-server"),
-              args: ["--stdio"],
+      switch (config.get("adapter")) {
+        case "stdio":
+          serverOptions = {
+            options: {
+              env: Object.assign({}, process.env, {
+                ["CREDO_LSP_VERSION"]: await latestRelease(
+                  "credo-language-server"
+                ),
+              }),
+            },
+            command: context.asAbsolutePath("./bin/credo-language-server"),
+            args: ["--stdio"],
+          };
+          break;
+        case "tcp":
+          serverOptions = () => {
+            // Connect to language server via socket
+            let socket = require("net").connect({
+              host: "127.0.0.1",
+              port: config.get("port"),
+            });
+            let result: StreamInfo = {
+              writer: socket,
+              reader: socket,
             };
-            break;
-          case "tcp":
-            serverOptions = () => {
-              // Connect to language server via socket
-              let socket = require("net").connect({
-                host: "127.0.0.1",
-                port: config.get("port"),
-              });
-              let result: StreamInfo = {
-                writer: socket,
-                reader: socket,
-              };
-              return Promise.resolve(result);
-            };
-            break;
-          default:
-            throw new Error("boom");
-        }
-        const clientOptions: LanguageClientOptions = {
-          documentSelector: [{ scheme: "file", language: "elixir" }],
-        };
-
-        credoClient = new LanguageClient(
-          "elixir-tools.credo",
-          "Credo",
-          serverOptions,
-          clientOptions
-        );
-
-        // Start the credoClient. This will also launch the server
-        credoClient.start();
+            return Promise.resolve(result);
+          };
+          break;
+        default:
+          throw new Error("boom");
       }
+      const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "elixir" }],
+      };
+
+      credoClient = new LanguageClient(
+        "elixir-tools.credo",
+        "Credo",
+        serverOptions,
+        clientOptions
+      );
+
+      // Start the credoClient. This will also launch the server
+      credoClient.start();
     }
   }
 }
 
+async function activateNextLS(
+  context: vscode.ExtensionContext,
+  _mixfile: vscode.Uri
+) {
+  let config = vscode.workspace.getConfiguration("elixir-tools.nextls");
+
+  if (config.get("enable")) {
+    let serverOptions: ServerOptions;
+
+    switch (config.get("adapter")) {
+      case "stdio":
+        serverOptions = {
+          options: {
+            env: Object.assign({}, process.env, {
+              ["NEXTLS_VERSION"]: await latestRelease("next-ls"),
+            }),
+          },
+          command: context.asAbsolutePath("./bin/nextls"),
+          args: ["--stdio"],
+        };
+        break;
+      case "tcp":
+        serverOptions = () => {
+          // Connect to language server via socket
+          let socket = require("net").connect({
+            host: "127.0.0.1",
+            port: config.get("port"),
+          });
+          let result: StreamInfo = {
+            writer: socket,
+            reader: socket,
+          };
+          return Promise.resolve(result);
+        };
+        break;
+      default:
+        throw new Error("boom");
+    }
+    const clientOptions: LanguageClientOptions = {
+      documentSelector: [{ scheme: "file", language: "elixir" }],
+    };
+
+    nextLSClient = new LanguageClient(
+      "elixir-tools.nextls",
+      "NextLS",
+      serverOptions,
+      clientOptions
+    );
+
+    // Start the nextLSClient. This will also launch the server
+    nextLSClient.start();
+  }
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+  let files = await vscode.workspace.findFiles("mix.exs");
+
+  if (files[0]) {
+    await activateCredo(context, files[0]);
+    await activateNextLS(context, files[0]);
+  }
+}
+
 export function deactivate() {
-  if (!credoClient) {
+  if (!credoClient && !nextLSClient) {
     return undefined;
   }
-  return credoClient.stop();
+  if (credoClient) {
+    credoClient.stop();
+  }
+
+  if (nextLSClient) {
+    nextLSClient.stop();
+  }
+
+  return true;
 }
